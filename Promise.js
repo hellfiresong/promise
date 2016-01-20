81b205d9cdcd0de9041961217c2df26c1055c4f3
hellfiresong <hellfiresong.me@gmail.com>
2016-01-21 06:36:24 +0800

    Promise library wrapper structure.

---
+++ Promise.js
@@ -0,0 +1,51 @@
+/**
+ * Build Your Own Promise.
+ * A lightweighted library
+ * For Promise standards, I follow ECMAScript language specification ECMA-262 6th edition(http://people.mozilla.org/~jorendorff/es6-draft.html#sec-promise-objects).
+ * Reference: (http://www.html5rocks.com/en/tutorials/es6/promises/).
+ *
+ * Features that the library contains:
+ *     parallel promise 
+ * 
+ * What I haven't done yet is test code, I will use mocha to write some case later.
+ *
+ * hellfire (asyncloading#163.com)
+ * Jan 21st, 2016
+ */
+
+(function(root) {
+
+
+  /* Promise constructor */
+  function Promise(fn) {
+  
+  }
+
+
+  /* Static Methods */
+  Promise.all = function() {
+  
+  };
+
+  Promise.resolve = function(value) {
+  
+  };
+
+  Promise.reject = function(value) {
+  
+  };
+
+  Promise.race = function(values) {
+  
+  }
+
+  /* Prototype methods */
+  Promise.prototype.then = function(onFulfilled, onRejected) {
+  
+  };
+
+  Promise.prototype.catch = function(onRejected) {
+  
+  };
+
+})(this);
