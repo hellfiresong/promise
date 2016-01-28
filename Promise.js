8abcf6432bd802789b3909c790495ca5110330d1
liufeng.clf <liufeng.clf@alibaba-inc.com>
2016-01-28 21:32:26 +0800

    Update comments

--- Promise.js
+++ Promise.js
@@ -1,24 +1,39 @@
 /**
  * Build Your Own Promise.
  * A lightweighted library
- * For Promise standards, I follow ECMAScript language specification ECMA-262 6th edition(http://people.mozilla.org/~jorendorff/es6-draft.html#sec-promise-objects).
- * Open source Promise/A+ standard(https://promisesaplus.com/)
- * Reference: (http://www.html5rocks.com/en/tutorials/es6/promises/).
- * How to realize resolve, reject, then base of https://github.com/promises-aplus/promises-spec
+ * I think if you want to build your own promise, the main part is to maintain a state machine, you should know how to transform state between pending,
+ * fulfilled and rejected.
+ *
+ * Promise standard:
+ *     Promise/A+
+ *     
+ * Reference: 
+ *     H5 rocks promise tutorials(http://www.html5rocks.com/en/tutorials/es6/promises/)
+ *     Open source Promise/A+ standard(https://promisesaplus.com/)
+ *     ECMAScript language specification ECMA-262 6th edition promise part(http://people.mozilla.org/~jorendorff/es6-draft.html#sec-promise-objects).
+ *
+ * Realize step:
+ *     How to realize resolve, reject, then base on https://github.com/promises-aplus/promises-spec
  *
  * Features that the library contains:
- *     parallel promise 
- * 
- * What I haven't done yet is test code, I will use mocha to write some case later.
+ *     parallel promise
+ *
+ * Support: 
+ *     Both Browser and Node.js environment.
+ *
+ * To do...
+ *     I will use mocha to write some case later.
+ *
  *
  * hellfire (asyncloading#163.com)
- * Jan 21st, 2016
+ * Jan 28th, 2016
  */
 
 (function(root) {
 
-  /* Helper */
+  /* Some utilities function. */
 
+  /* Noop function. */
   var noop = function() {};
 
   /* Check a value is an array or not. */
@@ -26,11 +41,11 @@
     return Object.prototype.toString.call(value) === "[object Array]";
   }
   
-  
-  /* Check wether an object has then method. */
+  /* If obj is thenable or not */
   var isThenable = function(obj) {
     return obj && typeof obj.then === 'function';
   };
+
 
 
   /* Promise three kinds of state */
@@ -42,6 +57,12 @@
   /**
    * Promise constructor
    * Maintain a state machine.
+   *
+   * Notes:
+   *    1,  Any promise object is in one of three mutually states: fulfilled, rejected and pending.
+   *    2,  Pending state can transform to fulfilled or rejected state.
+   *    3,  State can't transform between fulfilled and rejected.
+   *    
    */
   function Promise(fn) {
     
@@ -55,8 +76,7 @@
     
     this._state = PENDING;              /* Promise state */
     this._value = undefined;            /* Return value */  
-    this._deferreds = [];               /* Store success && failure handlers caused by then*/
- 
+    this._deferreds = [];               /* Store success && failure handlers passed by then*/ 
   }
 
   function resolve(promise, x) {
@@ -95,12 +115,38 @@
   }
 
   /* Prototype methods */
-  Promise.prototype.then = function(onFulfilled, onRejected) {
+
   
+  /**
+   * The then method.
+   *
+   * @param {onFulfilled}   optional arguments, must be function type. 
+   * @param {onRejected}    optional arguments, must be function type.
+   *
+   * @return                a promise object.
+   */
+  Promise.prototype.then = function(onFulfilled, onRejected) {
+    /* Arguments function type check */
+    if (typeof onFulfilled !== 'function') {
+      onFulfilled = null;
+    }
+    if (typeof onRejected !== 'function') {
+      onRejected = null;
+    }
+
+    var promise = new Promise(noop);  
+    
   };
 
   Promise.prototype.catch = function(onRejected) {
   
   };
+
+  /* Browser and Node.js environment support. */
+  if (typeof module !== 'undefined' && modue.exports) {
+    module.exports = Promise;
+  } else if (!root.Promise) {
+    root.Promise = Promise;
+  }
 
 })(this);
